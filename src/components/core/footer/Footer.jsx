a3bfbdbc417a2d1c1ea438166dcab25a7d3b0247
mdameerhamza <mdameerhamza91@gmail.com>
2024-01-26 11:39:38 +0500

    desktop version, home,login,register,about,contact

--- src/components/core/footer/Footer.jsx
+++ src/components/core/footer/Footer.jsx
@@ -1,9 +1,70 @@
+import { Link } from "react-router-dom";
 import "./footer.css";
+import callIcon from "../../../assets/images/call.svg";
+import mailIcon from "../../../assets/images/mail.svg";
+import locationIcon from "../../../assets/images/foot-location.svg";
+import logo from "../../../assets/images/logo.svg";
 
 const Footer = () => {
     return(
         <>
-            <footer id="footer"><h1>footer</h1></footer>
+            <footer id="footer">
+            <div class="container">
+                <div class="information">
+                    <div class="info">
+                        <div class="info-img">
+                            <img src={callIcon} alt="" />
+                        </div>
+                        <div class="info-content">
+                            <span>Phone:</span>
+                            <p>513,603,2999</p>
+                        </div>
+                    </div>
+                    <div class="info">
+                        <div class="info-img">
+                            <img src={mailIcon} alt="" />
+                        </div>
+                        <div class="info-content">
+                            <span>Email:</span>
+                            <p>contactus@senneca.com</p>
+                        </div>
+                    </div>
+                    <div class="info">
+                        <div class="info-img">
+                            <img src={locationIcon} alt="" />
+                        </div>
+                        <div class="info-content">
+                            <span>Address:</span>
+                            <p>11502 Century BoulevardCincinnati, OH 452</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-nav">
+                    <div class="foot-logo">
+                        <Link to="/"><img src={logo} alt="ScoreTickets" /></Link>
+                    </div>
+                    <div class="foot-nav">
+                        <nav id="navbar">
+                            <ul>
+                                <li><Link to="/" class="active">Sports</Link></li>
+                                <li><Link to="/">Concerts</Link></li>
+                                <li><Link to="/">Shows</Link></li>
+                                <li><Link to="/">Cities</Link></li>
+                                <li><Link to="/">Support</Link></li>
+                                <li><Link to="/">Sell</Link></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="social">
+                        <img src="images/Social Icons.svg" alt="" />
+                    </div>
+                </div>
+                <div class="copy">
+                    <p>Copyright © 2023, ScoreSpecs. All Rights Reserved.</p>
+                    <span><a href="#.">Privacy Policy </a> &nbsp;  |   &nbsp;<a href="#."> Term of use</a></span>
+                </div>
+            </div>
+        </footer>
         </>
     )
 }
--- src/pages/home/Index.jsx
+++ src/pages/home/Index.jsx
@@ -5,19 +5,22 @@ import image1 from "../../assets/images/img1.png";
 import image2 from "../../assets/images/img2.png";
 import image3 from "../../assets/images/img3.png";
 import arrow from "../../assets/images/arrow.png";
+import image4 from "../../assets/images/img4.png";
+import image5 from "../../assets/images/img5.png";
+import image6 from "../../assets/images/img6.png";
+import locationIcon from "../../assets/images/location-icon.png";
+import promoImage from "../../assets/images/promo-image.svg";
+import image7 from "../../assets/images/img7.png";
+import image8 from "../../assets/images/img8.png";
 import { Link } from "react-router-dom";
-import Slider from "react-slick";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
 
 
 
 const Home = () => {
-    var settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-      };
     return(
         <>
             <div className="home-page">
@@ -58,29 +61,174 @@ const Home = () => {
                     <section className="cols">
                         <div className="container flex column-direction">
                             <h2 className="sec-heading">Featured Events On Sale Today</h2>
-                            <div className="columns flex">
-                                <Slider {...settings}>
-                                    <div>
-                                        <h3>1</h3>
+                            <Swiper
+                                navigation
+                                pagination={false}
+                                slidesPerView={3}
+                                autoplay={{
+                                    delay: 3000, // Adjust the delay as needed (in milliseconds)
+                                    disableOnInteraction: false, // Set to false to keep autoplaying even when user interacts with the slider
+                                  }}
+                                className="mySwiper"
+                            >
+
+                                <SwiperSlide>
+                                    <div className="slide-item">
+                                        <img src={image4} alt="img"/>
+                                        <h2>Billy Joel & Stevie Nicks</h2>
+                                        <span className="location">
+                                            <img src={locationIcon} alt="#" />
+                                            New York, United States
+                                        </span>
+                                        <strong className="price">From $80</strong>
                                     </div>
-                                    <div>
-                                        <h3>2</h3>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <div className="slide-item">
+                                        <img src={image5} alt="img"/>
+                                        <h2>Billy Joel & Stevie Nicks</h2>
+                                        <span className="location">
+                                            <img src={locationIcon} alt="#" />
+                                            New York, United States
+                                        </span>
+                                        <strong className="price">From $80</strong>
                                     </div>
-                                    <div>
-                                        <h3>3</h3>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <div className="slide-item">
+                                        <img src={image6} alt="img"/>
+                                        <h2>Billy Joel & Stevie Nicks</h2>
+                                        <span className="location">
+                                            <img src={locationIcon} alt="#" />
+                                            New York, United States
+                                        </span>
+                                        <strong className="price">From $80</strong>
                                     </div>
-                                    <div>
-                                        <h3>4</h3>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <div className="slide-item">
+                                        <img src={image4} alt="img"/>
+                                        <h2>Billy Joel & Stevie Nicks</h2>
+                                        <span className="location">
+                                            <img src={locationIcon} alt="#" />
+                                            New York, United States
+                                        </span>
+                                        <strong className="price">From $80</strong>
                                     </div>
-                                    <div>
-                                        <h3>5</h3>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <div className="slide-item">
+                                        <img src={image5} alt="img"/>
+                                        <h2>Billy Joel & Stevie Nicks</h2>
+                                        <span className="location">
+                                            <img src={locationIcon} alt="#" />
+                                            New York, United States
+                                        </span>
+                                        <strong className="price">From $80</strong>
                                     </div>
-                                    <div>
-                                        <h3>6</h3>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <div className="slide-item">
+                                        <img src={image6} alt="img"/>
+                                        <h2>Billy Joel & Stevie Nicks</h2>
+                                        <span className="location">
+                                            <img src={locationIcon} alt="#" />
+                                            New York, United States
+                                        </span>
+                                        <strong className="price">From $80</strong>
                                     </div>
-                                </Slider>
-                            </div>
+                                </SwiperSlide>
+                            </Swiper>
                             <div className="link-holder"><Link to="/all-events">View Events <img src={arrow} alt="->" /></Link></div>
+                        </div>
+                        
+                    </section>
+                    <section className="cols">
+                        <div className="container flex column-direction">
+                            <h2 className="sec-heading">Popular Venues</h2>
+                            <Swiper
+                                navigation
+                                autoplay={{
+                                    delay: 3000, // Adjust the delay as needed (in milliseconds)
+                                    disableOnInteraction: false, // Set to false to keep autoplaying even when user interacts with the slider
+                                  }}
+                                pagination={false}
+                                slidesPerView={2}
+                                className="mySwiper"
+                            >
+
+                                {/* <SwiperSlide>
+                                    <div className="slide-item">
+                                        <img src={image4} alt="img"/>
+                                        <h2>Billy Joel & Stevie Nicks</h2>
+                                        <span className="location">
+                                            <img src={locationIcon} alt="#" />
+                                            New York, United States
+                                        </span>
+                                        <strong className="price">From $80</strong>
+                                    </div>
+                                </SwiperSlide> */}
+                                <SwiperSlide>
+                                    <div className="slide-item">
+                                        <img src={image8} alt="img"/>
+                                        <h2>Billy Joel & Stevie Nicks</h2>
+                                        <span className="location">
+                                            <img src={locationIcon} alt="#" />
+                                            New York, United States
+                                        </span>
+                                        <strong className="price">From $80</strong>
+                                    </div>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <div className="slide-item">
+                                        <img src={image7} alt="img"/>
+                                        <h2>Billy Joel & Stevie Nicks</h2>
+                                        <span className="location">
+                                            <img src={locationIcon} alt="#" />
+                                            New York, United States
+                                        </span>
+                                        <strong className="price">From $80</strong>
+                                    </div>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <div className="slide-item">
+                                        <img src={image7} alt="img"/>
+                                        <h2>Billy Joel & Stevie Nicks</h2>
+                                        <span className="location">
+                                            <img src={locationIcon} alt="#" />
+                                            New York, United States
+                                        </span>
+                                        <strong className="price">From $80</strong>
+                                    </div>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <div className="slide-item">
+                                        <img src={image7} alt="img"/>
+                                        <h2>Billy Joel & Stevie Nicks</h2>
+                                        <span className="location">
+                                            <img src={locationIcon} alt="#" />
+                                            New York, United States
+                                        </span>
+                                        <strong className="price">From $80</strong>
+                                    </div>
+                                </SwiperSlide>
+                            </Swiper>
+                            <div className="link-holder"><Link to="/all-events">View Events <img src={arrow} alt="->" /></Link></div>
+                        </div>
+                    </section>
+                    <section class="news-section">
+                        <div class="container">
+                            <div class="news-area">
+                                <div class="newsletter">
+                                    <h3>Join the <span>newsletter</span> to learn about future events</h3>
+                                    <p>Join the newsletter to and get 10% discount on your first ticket purchase.</p>
+                                    <input type="email" placeholder="Enter Email Address" />
+                                    <a href="#.">Subscribe</a>
+                                </div>
+                                <div class="news-img">
+                                    <img src={promoImage} alt="" />
+                                </div>
+                            </div>
                         </div>
                     </section>
                 </main>
